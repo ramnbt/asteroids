7261829ffc596fc9614c82c75b56bbc77a6d4c5d
ramnbt <sriramv@berkeley.edu>
2025-09-10 17:00:56 -0500

    Made constants, circleshape, and player func)

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
 
+    my_player = Player(x, y)
+
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
             
         screen.fill("black")
+        my_player.draw(screen)
         pygame.display.flip()
+        dt = (clock.tick(60))/1000
+    
 
-    print("Starting Asteroids!")
+    """print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    print(f"Screen height: {SCREEN_HEIGHT}")"""
+
     
 
 
